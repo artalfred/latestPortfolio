5d6c0a8f417b0fc56d7993c72a5d79fa11190b88
Art Alfred <bernalesartalfred@gmail.com>
2025-12-18 00:49:35 +0800

    Header

--- app/header.tsx
+++ app/header.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Button from "./components/Button";
-import Container from "./components/Container";
 import Menu from "./components/Menu";
 
 import Logo from "@/public/Logo/Logo.svg";
 
 export default function Header() {
   return (
-    <div className="mx-auto 2xl:px-40 lg:px-20 md:px-10 sm:px-10 px-5 mt-6 fixed w-full z-10 shadow-2xl">
+    <div className="mx-auto 2xl:px-40 lg:px-20 md:px-10 sm:px-10 px-5 mt-6 fixed w-full z-10">
       <div className=" w-full 2xl:px-8 lg:px-8 md:px-8 sm:p-2 p-2 rounded-full bg-sky-700/25 backdrop-blur-lg flex justify-between items-center nav">
         <div className="grid 2xl:pl-0 lg:pl-0 md:pl-0 sm:pl-4 pl-4">
           <div className="grid">
