cdcb8d415f29d9c662c1bcb22756789b3b0e9d9a
Art Alfred <bernalesartalfred@gmail.com>
2025-12-18 00:34:19 +0800

    Added New Logo for Mobile

--- app/header.tsx
+++ app/header.tsx
@@ -1,19 +1,27 @@
+import Image from "next/image";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Menu from "./components/Menu";
 
+import Logo from "@/public/Logo/Logo.svg";
+
 export default function Header() {
   return (
     <div className="mx-auto 2xl:px-40 lg:px-20 md:px-10 sm:px-10 px-5 mt-6 fixed w-full z-10 shadow-2xl">
-      <div className=" w-full 2xl:px-8 lg:px-8 md:px-8 sm:p-2 p-2 rounded-full bg-[#050a30]/25 backdrop-blur-lg flex justify-between items-center nav">
+      <div className=" w-full 2xl:px-8 lg:px-8 md:px-8 sm:p-2 p-2 rounded-full bg-sky-700/25 backdrop-blur-lg flex justify-between items-center nav">
         <div className="grid 2xl:pl-0 lg:pl-0 md:pl-0 sm:pl-4 pl-4">
           <div className="grid">
-            <h3 className="2xl:text-2xl lg:text-2xl md:text-xl sm:text-lg text-lg font-medium">
-              Fred Bernales
-            </h3>
-            <h3 className="2xl:text-sm lg:text-lg md:text-md sm:text-sm text-sm">
-              Web Developer · AI Automation
-            </h3>
+            <div className="2xl:block lg:block md:block sm:hidden hidden">
+              <h3 className="2xl:text-2xl lg:text-2xl md:text-xl sm:text-lg text-lg font-medium">
+                Fred Bernales
+              </h3>
+              <h3 className="2xl:text-sm lg:text-lg md:text-md sm:text-sm text-sm">
+                Web Developer · AI Automation
+              </h3>
+            </div>
+            <div className="2xl:hidden lg:hidden md:hidden sm:block block">
+              <Image src={Logo} alt="Bernales.logo.aa" width={50} height={50} />
+            </div>
           </div>
         </div>
 
